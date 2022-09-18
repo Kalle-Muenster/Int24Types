8bd95ea5ceb95808a19e49c6d49d47a8f4609672
Kalle Muenster <kalle@froglogic.com>
2022-09-19 00:28:18 +0200

    update enumoperators.h

--- etc/enumoperators.h
+++ etc/enumoperators.h
@@ -6,15 +6,16 @@
 ||                                                           ||
 \*\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
 #ifndef _enumoperators_h_
-#define _enumoperators_h_ (0x00000102)
+#define _enumoperators_h_ (0x00000103)
 
+#include <settings.h>
 
-
+#ifndef ENUM_OPERATOR_NAMESPACE
 #define ENUM_OPERATOR_NAMESPACE enum_utils
 //#define DO_NOT_INLINE_OPERATORS (1)
 //#define APPEND_TILDE_FOR_ANDNOT (1)
-//#define DO_NOT_COUNT_NULL_VALID (1)
-
+//#define TREAT_NULL_VALUES_VALID (1)
+#endif
 
 #ifndef EMPTY
 #define EMPTY_(T) ((T)-1)
@@ -47,23 +48,21 @@
 #endif
 #endif
 
-#ifdef  DO_NOT_COUNT_NULL_VALID
-#define VALIDITY(check) check > NULL && check
+#ifdef  TREAT_NULL_VALUES_VALID
+#define VALIDITY( check ) check  
 #else
-#define VALIDITY(check) check
+#define VALIDITY( check ) check > NULL && check
 #endif
 
 #ifdef  APPEND_TILDE_FOR_ANDNOT
 #define ANDOPT(value) & ~##value
-#define NOTAND_ASSIGN(value) &= value
+#define NOTAND_ASSIGN(var,val) var = eType( var & val )
 #else
 #define ANDOPT(value) & value
-#define NOTAND_ASSIGN(value) &= ~##value
+#define NOTAND_ASSIGN(var,val) var = eType( var & ~##val )
 #endif
 
-#ifdef  USE_NAMESPACER
-#include <WaveLib.inl/namespacer.h>
-#elif defined(ENUM_OPERATOR_NAMESPACE)
+#ifdef  ENUM_OPERATOR_NAMESPACE
 #define BEGIN_NAMESPACE namespace ENUM_OPERATOR_NAMESPACE {
 #define ENDOF_NAMESPACE }
 #else
@@ -74,66 +73,65 @@
 
 BEGIN_NAMESPACE
 
-    // value checks for types where enum constants can derive from
+    // value checks for all types where enum constants could be derived from:
     DECLARE_OPERATOR bool
-    is_val(unsigned int This) {
-        return ( VALIDITY(This) < EMPTY_(unsigned int) );
-    }
-    DECLARE_OPERATOR bool
-    is_val(unsigned char This) {
+    is_val( unsigned char This ) {
         return ( VALIDITY(This) < EMPTY_(unsigned char) );
     }
     DECLARE_OPERATOR bool
-    is_val(unsigned long This) {
-        return ( VALIDITY(This) < EMPTY_(unsigned long) );
-    }
-    DECLARE_OPERATOR bool
-    is_val(unsigned short This) {
+    is_val( unsigned short This ) {
         return ( VALIDITY(This) < EMPTY_(unsigned short) );
     }
     DECLARE_OPERATOR bool
-    is_val(unsigned longi This) {
-        return (VALIDITY(This) < EMPTY_(unsigned longi));
+    is_val( unsigned int This ) {
+        return ( VALIDITY(This) < EMPTY_(unsigned int) );
+    }
+    DECLARE_OPERATOR bool
+    is_val( unsigned long This ) {
+        return ( VALIDITY(This) < EMPTY_(unsigned long) );
+    }
+    DECLARE_OPERATOR bool
+    is_val( unsigned longi This ) {
+        return ( VALIDITY(This) < EMPTY_(unsigned longi) );
     }
 
-    // ..and corresponding negations of these
+    // negations corresponding to these value checks of types where enums can derive from
     DECLARE_OPERATOR bool
-    is_not(unsigned int notThis) {
+    is_not( unsigned char notThis ) {
         return !is_val( notThis );
     }
     DECLARE_OPERATOR bool
-    is_not(unsigned short notThis) {
-        return !is_val(notThis);
+    is_not( unsigned short notThis ) {
+        return !is_val( notThis );
     }
     DECLARE_OPERATOR bool
-    is_not(unsigned long notThis) {
-        return !is_val(notThis);
+    is_not( unsigned int notThis ) {
+        return !is_val( notThis );
     }
     DECLARE_OPERATOR bool
-    is_not(unsigned char notThis) {
-        return !is_val(notThis);
+    is_not( unsigned long notThis ) {
+        return !is_val( notThis );
     }
     DECLARE_OPERATOR bool
-    is_not(unsigned longi notThis) {
-        return !is_val(notThis);
+    is_not( unsigned longi notThis ) {
+        return !is_val( notThis );
     }
 
-    // and for enum type variables...
+    // value chack templates for enum type variables:
     
     // is_val(enumvar) returns true on valid variables
     template<typename eType> DECLARE_OPERATOR bool
-        is_val(eType value) {
+        is_val( eType value ) {
         return value != EMPTY_(eType);
     }
     // is_not(enumvar) returns true on EMPTY variables
     template<typename eType> DECLARE_OPERATOR bool
-        is_not(eType value) {
+        is_not( eType value ) {
         return value == EMPTY_(eType);
     }
 
 
-
-    // masking:
+    // bitwise operators:
     //--------------------------------------------------//
 
     // operator '|' performs regular bitwise OR operation
@@ -141,13 +139,13 @@ BEGIN_NAMESPACE
         operator |(eType eTval, eType mask) {
         return eType((ulong)eTval | (ulong)mask);
     }
-    // operator '|=' add flags (mask parameter) to a variable
+    // operator '|=' add flags 'mask' to a variable
     template<typename eType> DECLARE_OPERATOR eType&
         operator |=(eType& eTval, eType mask) {
         eTval = eType((ulong)eTval | (ulong)mask);
         return eTval;
     }
-    // operator '^' bitwise exclusivive or operation
+    // operator '^' bitwise exclusivive OR operation
     template<typename eType> DECLARE_OPERATOR eType
         operator ^(eType eTval, eType mask) {
         return eType((ulong)eTval ^ (ulong)mask);
@@ -164,17 +162,17 @@ BEGIN_NAMESPACE
         return eType((ulong)eTval & (ulong)mask);
     }
 
-    // operator '&=' removes any bits not matching to the
+    // operator '&=' removes any bits not matching the
     // given bitmask. implements bitwise AND 'assignment' 
 	// Or (if APPEND_TILDE_FOR_ANDNOT is defined) then
-	// implements as bitwise NOT_AND 'assignment' instead    
+	// implements as bitwise ANDNOT 'assignment' instead    
     template<typename eType> DECLARE_OPERATOR eType&
         operator &=(eType& eTval, eType mask) {
-        eTval = eType((ulong)eTval ANDOPT(mask));
+        eTval = eType( eTval ANDOPT( mask ) );
         return eTval;
     }
 
-    // aritmetic:
+    // aritmetic operators:
     //--------------------------------------------------//
     
     // modulo
@@ -193,7 +191,7 @@ BEGIN_NAMESPACE
         operator +(eType value, eType add) {
         return eType(value + (slong)add);
     }
-    // additioning assignment
+    // accumulative assignment
     template<typename eType> DECLARE_OPERATOR eType&
         operator +=(eType& value, slong incrementor) {
         value = eType((slong)value + incrementor);
@@ -211,34 +209,34 @@ BEGIN_NAMESPACE
         return value;
     }
 
-    // higherlevel flags handling:
-    //----------------------------------------------//
+    // higherlevel flag handling templates using operators from above:
+    //--------------------------------------------------------------//
 
-    // 'variable' contains ALL bits of 'exactMatch'
+    // 'variable' contains ALL bits of 'exact' match
     template<typename eType> DECLARE_OPERATOR bool
         hasFlag(eType variable, eType exact) {
-        return exact && exact == (variable & exact);
+        return is_val(exact) && exact==(variable & exact);
     }
     // 'variable' contains ANY 'ofThese' several bits
     template<typename eType> DECLARE_OPERATOR bool
         anyFlag(eType ofThese, eType inVariable) {
-        return (ofThese & inVariable) != 0;
+        return (ofThese & inVariable) != eType(0);
     }
-    // 'set' passed flag bits on the passed variable
+    // set 'allThese' bits in the passed 'variable'  
     template<typename eType> DECLARE_OPERATOR eType
-        addFlag(eType& variable, eType addflags) {
-        return variable |= addflags;
+        addFlag(eType& variable, eType allThese) {
+        return variable |= allThese;
     }
-    // 'remove' passed flag bits from passed variable
+    // unset 'allThese' bits on the passed 'variable'
     template<typename eType> DECLARE_OPERATOR eType
-        remFlag(eType& variable, eType flagmask) {
-        return variable NOTAND_ASSIGN( flagmask );
+        remFlag( eType& variable, eType allThese ) {
+        return NOTAND_ASSIGN( variable, allThese );
     }
 
 ENDOF_NAMESPACE
 
 //---
-//prevent global namespace from gaining pollution from macros just locally needed
+//prevent global namespace from getting polluted with macros just locally needed
 //...
 #undef DECLARE_OPERATOR
 #undef NOTAND_ASSIGN
